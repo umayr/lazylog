adadc75b0ec31fe6c6d14c556b46501600a89fb1
Umayr Shahid <umayrr@hotmail.co.uk>
2015-03-02 16:56:33 +0500

    Pandora's box. Shh.

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Umayr Shahid on 15/02/15.
+ */
+
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+    SHA1_REGEX: /\b([a-f0-9]{40})\b/i,
+    NUMBER: 10,
+    FORMAT: 'oneline'
+};
+
